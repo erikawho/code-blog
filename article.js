d2830a637a52cc587e3f746207b49fad10902523
ERIKAWHO <Erika@RefreshMediaResources.com>
2015-12-02 11:47:16 -0800

    changes to code-blog

--- article.js
+++ article.js
@@ -4,20 +4,56 @@ var Article = function(props) {
   this.category = props.category;
   this.title = props.title;
   this.body = props.body;
-  this.publishedOn = props.publishedOn;
-};
-Article.prototype.toHTML = function() {
-  return '<article>' +
-  '<h1>' + this.title + '</h1>' +
-  '<a>' + this.author + '</a>' +
-  '<a>' + this.authorURL + '</a>' +
-  '<h3>' + this.category + '</h3>' +
-  '<p>' + this.body + '</p>' +
-  '<footer>' + this.publishedOn + '</footer>' +
-    '</article>';
+  this.age = this.postAge(this.publishedOn);
+  blog.articles.push(this);
 };
 
-function (newArticle)
-var $(Article.copy) = $('#template').clone
-$('main').append(newArticle.toHTML());
-console.log(newArticle.title)
+Article.prototype.toHTML = function() {
+  var age = this.postAge(this.publishedOn);
+  var $clonedArticle = $('article#post').clone();
+$clonedArticle.removeAttr ('id');
+  $clonedArticle.find('h1.author').html ('<a href = "' + this.authorURL + '>' + this.author + '</a>');
+  $clonedArticle.find('h2.title').html(this.title);
+  $clonedArticle.find('div.post').html(this.body);
+  $clonedArticle.find('h3.publishdate').html(this.publishedOn);
+  $clonedArticle.find('h3.category').html(this.category);
+  $('main').append($clonedArticle);
+//Anonymous callback function
+
+Article.prototype.tagsDropDown = function() {
+  var $clonedMenuItem1 = $('.catMenuItem').clone();
+  $clonedMenuItem1.removeAttr('class');
+    $clonedMenuItem1.attr('value', this.category);
+      $clonedMenuItem1.text(this.category);
+      if ($"#catFilter select").find('option[value="' + this.category + '"]').length === 0) {
+        $('#catFilter select').append($clonedMenuItem1);
+      }
+
+Article.prototype.tagsDropDown = function() {
+  var $clonedMenuItem2 = $('.authMenuItem').clone();
+  $clonedMenuItem2.removeAttr('class');
+    $clonedMenuItem2.attr('value', this.author);
+      $clonedMenuItem2.text(this.author);
+      if ($"#authFilter select").find('option[value="' + this.author + '"]').length === 0) {
+        $('#authFilter select').append($clonedMenuItem2);
+            }
+}
+
+Article.prototype.postAge = function(date) {
+   var today = new Date();
+   var dd = parseInt(today.getDate());
+   var mm = parseInt(today.getMonth()+1);
+   var yyyy = parseInt(today.getFullYear());
+
+   var year = parseInt(date.slice(0,4));
+   var month = parseInt(date.slice(5,7));
+   var day = parseInt(date.slice(8,10));
+
+   var oneDay = 24*60*60*1000 //Hours*Minutes*Seconds*Milliseconds
+   var firstDate = new Date(year,month,day); //Publish date
+   var secondDate = new Date(yyyy,mm,dd); //Today
+
+   var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+   return diffDays;
+}
+// Will result in milliseconds
--- blog.js
+++ blog.js
@@ -1,2 +1,64 @@
 var blog = {};
 blog.articles = [];
+
+blog.createAll = function() {
+  this.rawData.sort(function(a, b) {
+    if (a.publishedOn > b.publishedOn) {return -1;}
+    if (a.publishedOn < b.publishedOn) {return 1;}
+    return 0;
+  });
+
+  for (var i = 0; i < this.rawData.length; i++) {
+    var temp = new Article(this.rawData[i]);
+    this.articles.push(temp);
+    temp.tagsDropDown();
+  }
+};
+
+blog.truncateArticles = function() {
+  $('article.postBody p:not(:first-child)').hide();
+  $('main').on('click', '.read-on', function(event) {
+    event.preventDefault();
+    $(this).parent().find('p:not(first-child)').show();
+    $(this).hide();
+  });
+};
+
+//We want to defer the implementation of the following code:
+$(document).ready(function() {  //The browser has the DOM ready. It has interpreted the HTML file, now onto the next thing.
+  blog.createAll();
+  $('#post').remove();
+  blog.truncateArticles();
+});
+
+$('select[id="category"]').change(function)){
+   $('#author').find('option:first').attr('selected', 'selected');
+   #('main').find('article').show();
+   console.log($(this).val());
+
+   if ($(this).val() !== 'none'){
+     $('postCategory:not(:contains(' + $(this).val() + '))').parent().hide();
+   }
+});
+
+$('select[id="author"]').change(function)){
+   $('#category').find('option:first').attr('selected', 'selected');
+   #('main').find('article').show();
+   console.log($(this).val());
+
+   if ($(this).val() !== 'none'){
+     $('article:not(:contains(' + $(this).val() + '))').hide();
+   }
+});
+
+  //Selects the entire document. Passes in the document object.
+
+  //Nav tabs
+  blog.handleMainNav = function() {
+     $('.main.-nav').on('click'. '.tab', function(e) {
+       $(.tab-content').hide();
+       $('#' + $(this).data('content')).fadeIn();
+     });
+     $('main-nav' .tab:first').trigger('click');
+
+     }
