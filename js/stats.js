61f90d6f24268b345a4da005e672dc139de9a5fa
ERIKAWHO <Erika@RefreshMediaResources.com>
2015-12-27 19:55:03 -0800

    Lined up stats functionality, works

--- js/stats.js
+++ js/stats.js
@@ -1,80 +1,75 @@
-// var stats = {};
-// stats.uniqueAuthors = [];
-// stats.markdownWordCounts = [];
-// stats.allWordsArray = [];
-// stats.eachAuthorWords = [];
-//
-// stats.getAllArticles = function(rawJSON) {
-//   stats.allArticles = rawJSON;
-// };
-//
-// stats.numOfArticles = function() {
-//   var articleCount = stats.allArticles.length;
-//   $('main').append('<p>Number of Articles: ' + articleCount + '</p>');
-// };
-//
-// stats.wordCount = function(str) {
-//   return str.replace(/[#,\n]/g,'').match(/\b\w+/g);
-// };
-//
-// stats.totalWordCount = function() {
-//   var allWords = function(art) {
-//     stats.markdownWordCounts.push(stats.wordCount(art.markdown).length);
-//   };
-//   stats.allArticles.map(allWords);
-//   stats.blogWordCount = stats.markdownWordCounts.reduce(getSum, 0);
-//   $('main').append('<p>Number of Words in All Articles: ' + stats.blogWordCount);
-//
-//   stats.numberOfAuthorWords = function() {
-// };
-//
-//
-//   stats.allWordLengths = function() {
-// };
-//
-// stats.searchAuthors = function() {
-//   // Search through array and if author is not there, add author to the article
-//   var getAuthors = function(art) {
-//     if ($.inArray(art.author, stats.uniqueAuthors) === -1) { // === "Equal value and equal type"
-//       stats.uniqueAuthors.push(art.author);
-//     }
-//   };
-//   stats.allArticles.map(getAuthors);
-//   // Map creates a new array with the same number of elements. Transformation of something that's in the original array. Modifying something that's available to the closure. What is the array going to look like that's returned to the map function? Array of undefined.
-// };
-//
-// stats.numOfAuthors = function() {
-//   var authorCount = stats.uniqueAuthors.length;
-//   $('main').append('<p>Number of Articles: ' + authorCount + '</p>');
-// };
-//
-// stats.numberOfAuthorWords = function() {
-//   stats.uniqueAuthors.forEach(function(element, index, array) {
-//     var countTemp = 0;
-//     var getAuthorWords = function(art) {
-//       if (art.author === element) {
-//         countTemp += stats.wordCount(art.markdown).length;
-//       };
-//     };
-//     stats.allArticles.forEach(getAuthorWords);
-//     stats.eachAuthorWords.push(countTemp);
-//   });
-//
-//   stats.uniqueAuthors.forEach(function(element, index, array) {
-//     $('main').append('<p>Number of Words ' + element + ' has written: </p>');
-//   });
-// };
-//
-// $(function() {
-//   $.getJSON('/util/articles.json', stats.getAllArticles)
-//   .done(stats.numOfArticles);
-//   .done(stats.searchAuthors);
-//   .done(stats.numOfAuthors);
-//   .done(stats.totalWordCount);
-//   .done(stats.allWordLengths);
-//   .done(stats.numberOfAuthorWords);
-// });
-//
-// stats.wordCount = function(str) {
-//   return str.replace(/[#,\n]/g,'').match(/\b\w+/g);
-// };
+var stats = {};
+stats.words = [];
+var uniqueAuthor;
+var totalWords;
+
+$.getJSON('/util/articles.json', function(data){
+  stats.data = data;
+})
+  .done(function(){
+    stats.totalArticles();
+    stats.totalAuthors();
+    stats.totalWords();
+    stats.avgOverallWordLength();
+    stats.wordsByAuthor();
+  });
+
+stats.avgOverallWordLength = function() {
+  var totalWordLength = stats.words.map(function(item) {
+    return item.split('').length;
+  });
+  var totalWordLengthNumber = totalWordLength.reduce(stats.count);
+  var averageWordLength = Math.round(totalWordLengthNumber/totalWords);
+  $('#avgOverall').html('<p>Average word length across all posts: ' + averageWordLength + ' letters</p>');
+};
+
+stats.count = function(a,b) {
+  return a + b;
+};
+
+stats.pluck = function(property, collection) {
+  return collection.map(function(e){
+    return e[property];
+  });
+};
+
+stats.totalArticles = function() {
+  $('#totalArticles').html('<p>Total articles: ' + stats.data.length + '</p>');
+};
+
+stats.totalAuthors = function() {
+  stats.author = [];
+  stats.author = stats.pluck('author', stats.data);
+  uniqueAuthor = $.unique(stats.author);
+  $('#totalAuthors').html('<p>Total authors: ' + uniqueAuthor.length + '</p>');
+};
+
+stats.totalWords = function() {
+  stats.words = stats.pluck('markdown', stats.data);
+  var wordCount = stats.words.map(function(item) {
+    return item.split(' ').length;
+  });
+  totalWords = wordCount.reduce(stats.count);
+  $('#totalWords').html('<p>Total words: ' + totalWords + '</p>');
+};
+
+stats.wordsByAuthor = function() {
+  for(var i=0; i<uniqueAuthor.length; i+=1) {
+    var authorMatch = uniqueAuthor[i];
+    var temp = [];
+    var wordCountByAuthor;
+    for (var j=0; j<stats.data.length; j+=1) {
+      var searchArrayForAuthor = stats.data[j].author.match(authorMatch);
+      if (searchArrayForAuthor !== null) {
+        wordCountByAuthor = (stats.data[j].markdown.split(' ')).length;
+        temp.push(wordCountByAuthor);
+      }
+    }
+    if (temp.length > 1) {
+      var totalWordsByAuthor = temp.reduce(stats.count);
+      $('#avgByAuthor').append('<p>Average words per article by ' + authorMatch + ': ' + totalWordsByAuthor/temp.length + ' words</p>');
+    } else {
+      $('#avgByAuthor').append('<p>Average words per article by ' + authorMatch + ': ' + wordCountByAuthor + ' words</p>');
+    }
+  }
+};
